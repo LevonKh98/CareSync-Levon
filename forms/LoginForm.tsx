3938d180e5373174c4950d346fdc4d5dcb2f057b
Nick <karenartyom2016@gmail.com>
2024-11-28 17:28:39 -0800

    Simple Login Page

--- forms/LoginForm.tsx
+++ forms/LoginForm.tsx
@@ -1,8 +1,16 @@
-import { Button } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, FormLabel } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 const LoginForm = () => {
-  return <Button>Login</Button>;
+  return (
+    <>
+      <Box marginBottom="100%" />
+      <FormLabel fontSize={50}>Staff Login</FormLabel>
+      <Input placeholder="Username" marginBottom={3} />
+      <Input placeholder="Password" marginBottom={3} />
+      <Button>Login</Button>
+    </>
+  );
 };
 
 export default LoginForm;
