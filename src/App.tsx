5dade152654a20661673a4a0da4069d86b8fcb34
Nick <karenartyom2016@gmail.com>
2025-02-13 21:40:58 -0800

    updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import StaffPage from "./frames/StaffPage";
 import AdminLogin from "./frames/AdminLogin";
 import AdminPage from "./frames/AdminPage";
 import Help from "./frames/Help";
-import ForgotPassword from "./components/ForgotPassword";
 import FAQ from "./frames/FAQ";
 
 function App() {
@@ -20,8 +19,6 @@ function App() {
         <Route path="/adminPage" element={<AdminPage />} />
 
         <Route path="/help" element={<Help />} />
-
-        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/help" element={<Help />} />
         <Route path="/faq" element={<FAQ />} />
       </Routes>
